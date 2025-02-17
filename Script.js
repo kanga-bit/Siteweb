666987667eb36eec06469f4a4fc9e84051e877ad
Juifa-Mode <kangamerveille7@gmail.com>
2025-02-17 17:42:26 +0100

    Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+document.getElementById('sample').addEventlistener('click', fonction (){
+const mer = document.getElementById('menu');
+mer.style.display = mer.style.display ==="block" ? "none" : "block";
+
+});
